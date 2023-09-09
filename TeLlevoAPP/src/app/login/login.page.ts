642de38a6b49d229436242f8e4c4d47ff0dc84e6
BenjaminAedo <118937298+BenjaminAedo@users.noreply.github.com>
2023-09-09 13:48:04 -0300

    .

--- TeLlevoAPP/src/app/login/login.page.ts
+++ TeLlevoAPP/src/app/login/login.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular//forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,8 +13,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+ 
+  formularioLogin: FormGroup; 
 
-  constructor() { }
+  constructor(public fb: FormBuilder) {
+    
+    this.formularioLogin = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    })
+
+    
+  }
 
   ngOnInit() {
   }
