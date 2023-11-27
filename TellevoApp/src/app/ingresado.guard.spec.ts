9c4eeb513fbe575038ebcbcc5765288d3edb78a7
GonzaloMontoya <127605011+GonzaloMontoya@users.noreply.github.com>
2023-11-27 00:03:34 -0300

    Pruebas guard que no funcionan

--- TellevoApp/src/app/ingresado.guard.spec.ts
+++ TellevoApp/src/app/ingresado.guard.spec.ts
@@ -1,16 +1,35 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router, RouterModule } from '@angular/router'; /*???*/
 
+import { LoginPage } from './login/login.page';
+import { StateService } from './state/state.service'; 
 import { IngresadoGuard } from './ingresado.guard';
 
 describe('IngresadoGuard', () => {
   let guard: IngresadoGuard;
+  let stateService: StateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[Router, RouterModule],
+      providers:[IngresadoGuard, StateService]
+    });
     guard = TestBed.inject(IngresadoGuard);
+    stateService = TestBed.inject(StateService);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
   });
+
+  it('deberia retornar true si el usuario no esta en blanco', ()=> {
+    spyOn(stateService, 'getUsuario').and.returnValue({usuario: 'Miranda'});
+    expect(guard.canActivate()).toBeTrue();
+  });
+
+  it('deberia retornar false si el usuario esta en blanco', () => {
+    spyOn(stateService, 'getUsuario').and.returnValue(null);
+    expect(guard.canActivate()).toBeFalse();
+  });
+
 });
--- TellevoApp/src/app/state/dummy.service.spec.ts
+++ TellevoApp/src/app/state/dummy.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { DummyService } from './dummy.service';
 
 describe('DummyService', () => {
   let service: DummyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
+
     service = TestBed.inject(DummyService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  
 });
