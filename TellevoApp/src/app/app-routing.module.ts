e1340f99b1bef4cd5b26f7496be5dd57c100d1be
GonzaloMontoya <127605011+GonzaloMontoya@users.noreply.github.com>
2023-12-08 17:39:19 -0300

    Arreglos en detalles y en letras

--- TellevoApp/src/app/app-routing.module.ts
+++ TellevoApp/src/app/app-routing.module.ts
@@ -33,9 +33,11 @@ const routes: Routes = [
     path: 'detalles',
     loadChildren: () => import('./detalles/detalles.module').then( m => m.DetallesPageModule),
     canActivate:[IngresadoGuard]
-  },  {
+  },
+  {
     path: 'solicitar',
-    loadChildren: () => import('./solicitar/solicitar.module').then( m => m.SolicitarPageModule)
+    loadChildren: () => import('./solicitar/solicitar.module').then( m => m.SolicitarPageModule),
+    canActivate:[IngresadoGuard]
   },
 
 ];
